3d8b7d9fff6a95eeabdf3ee968c2d78177d22728
decly <dec.ly@163.com>
2015-03-07 00:08:31 +0800

    add banner, todo: compatible

---
+++ js/main.js
@@ -0,0 +1,38 @@
+
+
+$(document).ready(
+	function() {
+	var size = $('.slidebtn>ul>li').length;
+	var idx = 0;
+	var swtimer;
+	
+	$('.slidebtn li').mouseover(function() {
+		idx = $('.slidebtn li').index(this);
+		showpic(idx);
+	}).eq(0).mouseover(); 
+
+	$('.slidebox').hover(function() {
+		if (swtimer)
+			clearInterval(swtimer);
+	}, function() {
+		swtimer = setInterval(function() {
+			showpic(idx);
+			idx++;
+			if (idx == size) {
+				idx = 0;
+			}
+		}, 4000);
+	}).trigger('mouseleave'); 
+
+	function showpic(idx) {
+		var perwidth = $('.slidepic img').width();
+		$('.slidepic').stop(true, false).animate({
+			left: -perwidth * idx
+		}, 500);
+		
+		$('.slidebtn li').removeClass('current').eq(idx).addClass('current');
+	}
+	
+});
+
+
