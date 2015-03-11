39dbdedc2057f59e8d3b815d78ce1f5ac5c50639
decly <dec.ly@163.com>
2015-03-11 23:19:21 +0800

    right menu, todo IE6

--- js/rmenu.js
+++ js/rmenu.js
@@ -1,32 +1,64 @@
 // for right menu
 $(document).ready(function() {
-	
+
 	var tophtml = "\
 	<div id=\"izl_rmenu\" class=\"izl-rmenu\"> \
 		<div class=\"btn btn-wx\"> \
-			<img class=\"wxpic\" src=\"img/weixin.jpg\"/> \
+			<img class=\"wxpic\" src=\"img/weixin_code.jpg\"/> \
 		</div> \
 		<div class=\"btn btn-mark\"> \
-			<div class=\"mark-info\"></div> \
+			<div class=\"mark-info\">添加到收藏</div> \
 		</div> \
+		<a target=\"_blank\" href=\"http://s.share.baidu.com/mshare?click=1&url=http%3A%2F%2Fwww.51arts.cn%2F&uid=0&to=mshare&type=text&pic=&title=%E5%A4%9A%E5%85%83%E4%B8%BB%E4%B9%89-%E9%9D%92%E9%93%9C%E6%97%B6%E4%BB%A3%E8%89%BA%E6%9C%AF%E5%93%81%E5%BA%97&key=&desc=&comment=&relateUid=&searchPic=0&sign=on&l=18tu7eees18tu7efe418tu7emsj&linkid=hy5n36qjpir&firstime=1403363766471\"> \
+			<div class=\"btn btn-share\"> 分享\
+				<div class=\"share-info\">分享给好友</div> \
+			</div> \
+		</a> \
+		<a target=\"_blank\" href=\"http://wpa.qq.com/msgrd?v=3&uin=2874311610&site=qq&menu=yes\"> \
+			<div class=\"btn btn-suggest\"> \
+				<div class=\"suggest-info\">欢迎您的反馈</div> \
+			</div> \
+		</a> \
 		<div class=\"btn btn-top\"></div> \
 	</div>";
 
 	$("#rmenu").html(tophtml);
-	//alert($("#rmenu").html());
 	$("#izl_rmenu").each(function() {
 		$(this).find(".btn-wx").mouseenter(function() {
 			$(this).find(".wxpic").fadeIn("fast");
-		});
-		$(this).find(".btn-wx").mouseleave(function() {
+		}).mouseleave(function() {
 			$(this).find(".wxpic").fadeOut("fast");
 		});
+
 		$(this).find(".btn-mark").mouseenter(function() {
 			$(this).find(".mark-info").fadeIn("fast");
-		});
-		$(this).find(".btn-mark").mouseleave(function() {
+		}).mouseleave(function() {
 			$(this).find(".mark-info").fadeOut("fast");
+		}).click(function() {
+			try {
+				if (document.all) { // IE
+					window.external.addFavorite(window.location, document.title);
+				} else {
+					window.sidebar.addPanel(document.title, window.location);
+				}
+			} catch (e) {
+				var ctrl = (navigator.userAgent.toLowerCase()).indexOf('mac') != -1 ? 'Command/Cmd' : 'CTRL';
+				alert('您可以使用快捷键 ' + ctrl + ' + D 添加到书签');
+			}
 		});
+
+		$(this).find(".btn-share").mouseenter(function() {
+			$(this).find(".share-info").fadeIn("fast");
+		}).mouseleave(function() {
+			$(this).find(".share-info").fadeOut("fast");
+		});
+
+		$(this).find(".btn-suggest").mouseenter(function() {
+			$(this).find(".suggest-info").fadeIn("fast");
+		}).mouseleave(function() {
+			$(this).find(".suggest-info").fadeOut("fast");
+		});
+
 		$(this).find(".btn-top").click(function() {
 			$("html, body").animate({
 				"scroll-top": 0
