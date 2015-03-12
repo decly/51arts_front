d4b65d924848c6106642954846989dc0c1153a4c
decly <dec.ly@163.com>
2015-03-12 23:49:42 +0800

    login, todo IE6

---
+++ js/main.js
@@ -0,0 +1,25 @@
+
+$(document).ready(function() {
+	$('#password').click(function(){
+		if (this.value == '密码')
+			this.value = '';
+	}).blur(function () {
+		if (this.value == '')
+			this.value = '密码';
+	});
+	$('#username').click(function(){
+		if (this.value == '会员名/手机号/邮箱')
+			this.value = '';
+	}).blur(function () {
+		if (this.value == '')
+			this.value = '会员名/手机号/邮箱';
+	});
+	$('#verify').click(function(){
+		if (this.value == '验证码')
+			this.value = '';
+	}).blur(function () {
+		if (this.value == '')
+			this.value = '验证码';
+	});
+	
+});
