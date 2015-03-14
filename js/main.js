ae02d9dba8e756b69dc584fe9cc74d4930720109
decly <dec.ly@163.com>
2015-03-14 18:46:11 +0800

    fix login for IE6

--- js/main.js
+++ js/main.js
@@ -1,33 +1,8 @@
-
-$(document).ready(function() {
-	
+$(document).ready(function() { //for login 	
 	$('#login_wrap input').focus(function() {
 		$(this).siblings('span').css({'display': 'none'});
 	}).blur(function() {
 		if (this.value.length == 0) 
 			$(this).siblings('span').css({'display': 'inline'});
 	});
-	
-//	$('#password').click(function(){
-//		if (this.value == '密码')
-//			this.value = '';
-//	}).blur(function () {
-//		if (this.value == '')
-//			this.value = '密码';
-//	});
-//	$('#username').click(function(){
-//		if (this.value == '会员名/手机号/邮箱')
-//			this.value = '';
-//	}).blur(function () {
-//		if (this.value == '')
-//			this.value = '会员名/手机号/邮箱';
-//	});
-//	$('#verify').click(function(){
-//		if (this.value == '验证码')
-//			this.value = '';
-//	}).blur(function () {
-//		if (this.value == '')
-//			this.value = '验证码';
-//	});
-	
 });
