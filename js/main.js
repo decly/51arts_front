f00050bfb9f713a9e81ae93db7856c8659f4b445
decly <dec.ly@163.com>
2015-03-14 18:36:25 +0800

    fix login

--- js/main.js
+++ js/main.js
@@ -1,25 +1,33 @@
 
 $(document).ready(function() {
-	$('#password').click(function(){
-		if (this.value == '密码')
-			this.value = '';
-	}).blur(function () {
-		if (this.value == '')
-			this.value = '密码';
+	
+	$('#login_wrap input').focus(function() {
+		$(this).siblings('span').css({'display': 'none'});
+	}).blur(function() {
+		if (this.value.length == 0) 
+			$(this).siblings('span').css({'display': 'inline'});
 	});
-	$('#username').click(function(){
-		if (this.value == '会员名/手机号/邮箱')
-			this.value = '';
-	}).blur(function () {
-		if (this.value == '')
-			this.value = '会员名/手机号/邮箱';
-	});
-	$('#verify').click(function(){
-		if (this.value == '验证码')
-			this.value = '';
-	}).blur(function () {
-		if (this.value == '')
-			this.value = '验证码';
-	});
+	
+//	$('#password').click(function(){
+//		if (this.value == '密码')
+//			this.value = '';
+//	}).blur(function () {
+//		if (this.value == '')
+//			this.value = '密码';
+//	});
+//	$('#username').click(function(){
+//		if (this.value == '会员名/手机号/邮箱')
+//			this.value = '';
+//	}).blur(function () {
+//		if (this.value == '')
+//			this.value = '会员名/手机号/邮箱';
+//	});
+//	$('#verify').click(function(){
+//		if (this.value == '验证码')
+//			this.value = '';
+//	}).blur(function () {
+//		if (this.value == '')
+//			this.value = '验证码';
+//	});
 	
 });
