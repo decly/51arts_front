d967dacbf3e1a21450f89a7d39a7989108567da2
decly <dec.ly@163.com>
2015-03-24 23:49:14 +0800

    index, fix rmenu

--- js/rmenu.js
+++ js/rmenu.js
@@ -3,18 +3,22 @@ $(document).ready(function() {
 
 	var tophtml = "\
 	<div id=\"izl_rmenu\" class=\"izl-rmenu\"> \
-		<div class=\"btn btn-wx\"> \
-			<img class=\"wxpic\" src=\"img/weixin_code.jpg\"/> \
-		</div> \
 		<div class=\"btn btn-mark\"> \
 			<div class=\"mark-info\">添加到收藏</div> \
 		</div> \
-		<a target=\"_blank\" href=\"http://s.share.baidu.com/mshare?click=1&url=http%3A%2F%2Fwww.51arts.cn%2F&uid=0&to=mshare&type=text&pic=&title=%E5%A4%9A%E5%85%83%E4%B8%BB%E4%B9%89-%E9%9D%92%E9%93%9C%E6%97%B6%E4%BB%A3%E8%89%BA%E6%9C%AF%E5%93%81%E5%BA%97&key=&desc=&comment=&relateUid=&searchPic=0&sign=on&l=18tu7eees18tu7efe418tu7emsj&linkid=hy5n36qjpir&firstime=1403363766471\"> \
-			<div class=\"btn btn-share\"> 分享\
+		<div class=\"btn btn-wx\"> \
+			<img class=\"wxpic\" src=\"img/weixin_code.jpg\"/> \
+		</div> \
+		<a target=\"_blank\" href=\"http://wpa.qq.com/msgrd?v=3&uin=2874311610&site=qq&menu=yes\"> \
+			<div class=\"btn btn-qq\"> \
+				<div class=\"qq-info\">联系我们</div> \
+			</div> \
+		</a> \
+		<a target=\"_blank\" href=\"http://s.share.baidu.com/mshare?click=1&url=http%3A%2F%2Fwww.51arts.cn%2F&uid=0&to=mshare&type=text&pic=&title=%E5%A4%9A%E5%85%83%E4%B8%BB%E4%B9%89-%E9%9D%92%E9%93%9C%E6%97%B6%E4%BB%A3%E8%89%BA%E6%9C%AF%E5%93%81%E5%BA%97&key=&desc=&comment=&relateUid=&searchPic=0&sign=on&l=18tu7eees18tu7efe418tu7emsj&linkid=hy5n36qjpir&firstime=1403363766471\"> \			<div class=\"btn btn-share\"> 分享\
 				<div class=\"share-info\">分享给好友</div> \
 			</div> \
 		</a> \
-		<a target=\"_blank\" href=\"http://wpa.qq.com/msgrd?v=3&uin=2874311610&site=qq&menu=yes\"> \
+		<a target=\"_blank\" href=\"#\"> \
 			<div class=\"btn btn-suggest\"> \
 				<div class=\"suggest-info\">欢迎您的反馈</div> \
 			</div> \
@@ -28,6 +32,12 @@ $(document).ready(function() {
 			$(this).find(".wxpic").fadeIn("fast");
 		}).mouseleave(function() {
 			$(this).find(".wxpic").fadeOut("fast");
+		});
+
+		$(this).find(".btn-qq").mouseenter(function() {
+			$(this).find(".qq-info").fadeIn("fast");
+		}).mouseleave(function() {
+			$(this).find(".qq-info").fadeOut("fast");
 		});
 
 		$(this).find(".btn-mark").mouseenter(function() {
