90a9cb42d2c87860c3cb64e9eab1a00a352874cc
decly <dec.ly@163.com>
2015-03-25 12:35:00 +0800

    index, sale and new, done

--- js/main.js
+++ js/main.js
@@ -33,41 +33,41 @@ $(document).ready(function() { // for artist signed
 });
 
 
-$(document).ready(function() { // text scroll for sale info
-	marquee(25, 10, 3000);
-
-	function marquee(height, speed, delay) {
-		var scrollT;
-		var pause = false;
-		var ScrollBox = document.getElementById("sale_scroll");
-		if (document.getElementById("holder").offsetHeight <= height) return;
-		var _tmp = ScrollBox.innerHTML.replace('holder', 'holder2');
-		ScrollBox.innerHTML += _tmp;
-		ScrollBox.onmouseover = function() {
-			pause = true
-		}
-		ScrollBox.onmouseout = function() {
-			pause = false
-		}
-		ScrollBox.scrollTop = 0;
-
-		function start() {
-			scrollT = setInterval(scrolling, speed);
-			if (!pause) ScrollBox.scrollTop += 2;
-		}
-
-		function scrolling() {
-			if (ScrollBox.scrollTop % height != 0) {
-				ScrollBox.scrollTop += 1;
-				if (ScrollBox.scrollTop >= ScrollBox.scrollHeight / 2) ScrollBox.scrollTop = 0;
-			} else {
-				clearInterval(scrollT);
-				setTimeout(start, delay);
-			}
-		}
-		setTimeout(start, delay);
-	}
-});
+//$(document).ready(function() { // text scroll for sale info
+//	marquee(25, 10, 3000);
+//
+//	function marquee(height, speed, delay) {
+//		var scrollT;
+//		var pause = false;
+//		var ScrollBox = document.getElementById("sale_scroll");
+//		if (document.getElementById("holder").offsetHeight <= height) return;
+//		var _tmp = ScrollBox.innerHTML.replace('holder', 'holder2');
+//		ScrollBox.innerHTML += _tmp;
+//		ScrollBox.onmouseover = function() {
+//			pause = true
+//		}
+//		ScrollBox.onmouseout = function() {
+//			pause = false
+//		}
+//		ScrollBox.scrollTop = 0;
+//
+//		function start() {
+//			scrollT = setInterval(scrolling, speed);
+//			if (!pause) ScrollBox.scrollTop += 2;
+//		}
+//
+//		function scrolling() {
+//			if (ScrollBox.scrollTop % height != 0) {
+//				ScrollBox.scrollTop += 1;
+//				if (ScrollBox.scrollTop >= ScrollBox.scrollHeight / 2) ScrollBox.scrollTop = 0;
+//			} else {
+//				clearInterval(scrollT);
+//				setTimeout(start, delay);
+//			}
+//		}
+//		setTimeout(start, delay);
+//	}
+//});
 
 
 $(document).ready(function() { // hover for items
