4c80d53497bcf7b1c6a1aed9567a508e09317d22
decly <dec.ly@163.com>
2015-03-25 15:45:17 +0800

    index, fix hot sale and new item, showbox done

--- js/activity.js
+++ js/activity.js
@@ -1,51 +1,51 @@
 // for activity
 
-$(document).ready(function() {
-	var defaultOpts = {
-		interval: 4000,
-		fadeInTime: 300,
-		fadeOutTime: 200
-	};
-	var _thumbs = $("#act ul.slide-li li");
-	var _bodies = $("#act ul.slide-pic li");
-	var _count = _thumbs.length;
-	var _current = 0;
-	var _intervalID = null;
-	var stop = function() {
-		window.clearInterval(_intervalID);
-	};
-	var slide = function(opts) {
-		if (opts) {
-			_current = opts.current || 0;
-		} else {
-			_current = (_current >= (_count - 1)) ? 0 : (++_current);
-		};
-		_bodies.filter(":visible").fadeOut(defaultOpts.fadeOutTime, function() {
-			_bodies.eq(_current).fadeIn(defaultOpts.fadeInTime);
-			_bodies.removeClass("current").eq(_current).addClass("current");
-		});
-		_thumbs.removeClass("current").eq(_current).addClass("current");
-	};
-	var go = function() {
-		stop();
-		_intervalID = window.setInterval(function() {
-			slide();
-		}, defaultOpts.interval);
-	};
-	var itemMouseOver = function(target, items) {
-		stop();
-		var i = $.inArray(target, items);
-		slide({
-			current: i
-		});
-	};
-	_thumbs.hover(function() {
-		if ($(this).attr('class') != 'current') {
-			itemMouseOver(this, _thumbs);
-		} else {
-			stop();
-		}
-	}, go);
-	_bodies.hover(stop, go);
-	go();
-});
+//$(document).ready(function() {
+//	var defaultOpts = {
+//		interval: 4000,
+//		fadeInTime: 300,
+//		fadeOutTime: 200
+//	};
+//	var _thumbs = $("#act ul.slide-li li");
+//	var _bodies = $("#act ul.slide-pic li");
+//	var _count = _thumbs.length;
+//	var _current = 0;
+//	var _intervalID = null;
+//	var stop = function() {
+//		window.clearInterval(_intervalID);
+//	};
+//	var slide = function(opts) {
+//		if (opts) {
+//			_current = opts.current || 0;
+//		} else {
+//			_current = (_current >= (_count - 1)) ? 0 : (++_current);
+//		};
+//		_bodies.filter(":visible").fadeOut(defaultOpts.fadeOutTime, function() {
+//			_bodies.eq(_current).fadeIn(defaultOpts.fadeInTime);
+//			_bodies.removeClass("current").eq(_current).addClass("current");
+//		});
+//		_thumbs.removeClass("current").eq(_current).addClass("current");
+//	};
+//	var go = function() {
+//		stop();
+//		_intervalID = window.setInterval(function() {
+//			slide();
+//		}, defaultOpts.interval);
+//	};
+//	var itemMouseOver = function(target, items) {
+//		stop();
+//		var i = $.inArray(target, items);
+//		slide({
+//			current: i
+//		});
+//	};
+//	_thumbs.hover(function() {
+//		if ($(this).attr('class') != 'current') {
+//			itemMouseOver(this, _thumbs);
+//		} else {
+//			stop();
+//		}
+//	}, go);
+//	_bodies.hover(stop, go);
+//	go();
+//});
--- js/main.js
+++ js/main.js
@@ -33,6 +33,23 @@ $(document).ready(function() { // for artist signed
 });
 
 
+// show box
+$(document).ready(function() {
+	$.showbox_in("#act"); // for activity
+	$.showbox_out("#hot_sale_showbox"); // for hot sale
+	$.showbox_out("#new_item_showbox"); // for new item
+});
+
+
+
+
+
+
+
+
+
+
+
 //$(document).ready(function() { // text scroll for sale info
 //	marquee(25, 10, 3000);
 //
