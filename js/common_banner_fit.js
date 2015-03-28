c2e82e84abe7e23fffec4d1fb9ada6456c40626f
decly <dec.ly@163.com>
2015-03-28 19:11:35 +0800

    art circle, done

--- js/common_banner_fit.js
+++ js/common_banner_fit.js
@@ -5,10 +5,6 @@ $(document).ready(function() {
 	var swtimer;
 	var interval = 4000;
 
-	if ($('#banner').hasClass("fullwidth")) {
-		$('#banner .slidepic').css("margin-left", -($('#banner .slidepic img').width() - $('#banner.slidebox').width()) / 2);
-	}
-
 	$('#banner .slidebtn li').mouseover(function() {
 		idx = $('#banner .slidebtn li').index(this);
 		showpic(idx);
@@ -20,21 +16,6 @@ $(document).ready(function() {
 	}, function() {
 		swtimer = setInterval(timer_showpic, interval);
 	}).trigger('mouseleave');
-
-	var ctlbtn = $('#banner.slidebox').find('#banner_pre, #banner_next');
-	if (ctlbtn.length > 0) {
-		ctlbtn.bind('click', function() {
-			var id = this.id;
-			if (id == 'banner_pre') {
-				idx--;
-				if (idx < 0) idx = size - 1;
-			} else {
-				idx++;
-				if (idx >= size) idx = 0;
-			}
-			showpic(idx);
-		});
-	}
 
 	function timer_showpic() {
 		idx++;
