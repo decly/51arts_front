06bb447f89e315af60580f3c8e971e4509a2cf47
decly <dec.ly@163.com>
2015-04-03 01:35:47 +0800

    fix artinfo, done

--- js/artinfo.js
+++ js/artinfo.js
@@ -1,13 +1,13 @@
 $(document).ready(function() { // hover for click sort
 
-	$('#click_sort ul li').mouseover(itemhover);
+	var _thumbs = $('#click_sort ul li');
+	_thumbs.mouseover(itemhover);
 
 	function itemhover() {
 		$(this).siblings().find('.click_sort_float').slideUp('fast');
 		$(this).find('.click_sort_float').slideDown('fast');
+		_thumbs.removeClass("current");
+		$(this).addClass("current");
 	}
 
 });
-
-
-
